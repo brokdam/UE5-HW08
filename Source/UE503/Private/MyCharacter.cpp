94bd17b11554a9b5b138ccdc39b620fdbf8fe560
BRD\brogd <brogdam208@kakao.com>
2026-04-22 12:38:57 +0900

    캐릭터 기본 동작 구현

--- Source/UE503/Private/MyCharacter.cpp
+++ Source/UE503/Private/MyCharacter.cpp
@@ -1,8 +1,11 @@
 //MyCharacter.cpp
 
 #include "MyCharacter.h"
+#include "MyPlayerController.h"
+#include "EnhancedInputComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 AMyCharacter::AMyCharacter()
 {
@@ -16,4 +19,130 @@ AMyCharacter::AMyCharacter()
 	Cameracomp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Cameracomp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 	Cameracomp->bUsePawnControlRotation = false;
+
+    NormalSpeed = 600.0f;
+    SprintSpeedMultiplier = 1.5f;
+    SprintSpeed = NormalSpeed * SprintSpeedMultiplier;
+
+    GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+}
+
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (AMyPlayerController* PlayerController = Cast<AMyPlayerController>(GetController()))
+		{
+            if (PlayerController->MoveAction)
+            {
+                EnhancedInput->BindAction(
+                    PlayerController->MoveAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AMyCharacter::Move
+                );
+            }
+
+            if (PlayerController->JumpAction)
+            {
+                EnhancedInput->BindAction(
+                    PlayerController->JumpAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AMyCharacter::StartJump
+                );
+
+                EnhancedInput->BindAction(
+                    PlayerController->JumpAction,
+                    ETriggerEvent::Completed,
+                    this,
+                    &AMyCharacter::StopJump
+                );
+            }
+
+            if (PlayerController->LookAction)
+            {
+                EnhancedInput->BindAction(
+                    PlayerController->LookAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AMyCharacter::Look
+                );
+            }
+
+            if (PlayerController->SprintAction)
+            {
+                EnhancedInput->BindAction(
+                    PlayerController->SprintAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AMyCharacter::StartSprint
+                );
+                EnhancedInput->BindAction(
+                    PlayerController->SprintAction,
+                    ETriggerEvent::Completed,
+                    this,
+                    &AMyCharacter::StopSprint
+                );
+            }
+		}
+	}
+}
+
+void AMyCharacter::Move(const FInputActionValue& value)
+{
+    if (!Controller) return;
+
+    const FVector2D MoveInput = value.Get<FVector2d>();
+
+    if (!FMath::IsNearlyZero(MoveInput.X))
+    {
+        AddMovementInput(GetActorForwardVector(), MoveInput.X);
+    }
+
+    if (!FMath::IsNearlyZero(MoveInput.Y))
+    {
+        AddMovementInput(GetActorRightVector(), MoveInput.Y);
+    }
+}
+
+void AMyCharacter::StartJump(const FInputActionValue& value)
+{
+    if (value.Get<bool>())
+    {
+        Jump();
+    }
+}
+
+void AMyCharacter::StopJump(const FInputActionValue& value)
+{
+    if (!value.Get<bool>())
+    {
+        StopJumping();
+    }
+}
+
+void AMyCharacter::Look(const FInputActionValue& value)
+{
+    FVector2D LookInput = value.Get<FVector2D>();
+    AddControllerYawInput(LookInput.X);
+    AddControllerPitchInput(LookInput.Y);
+}
+
+void AMyCharacter::StartSprint(const FInputActionValue& value)
+{
+    if (GetCharacterMovement())
+    {
+        GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+    }
+}
+
+void AMyCharacter::StopSprint(const FInputActionValue& value)
+{
+    if (GetCharacterMovement())
+    {
+        GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+    }
 }
--- Source/UE503/Public/MyCharacter.h
+++ Source/UE503/Public/MyCharacter.h
@@ -8,6 +8,7 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+struct FInputActionValue;
 
 UCLASS()
 class UE503_API AMyCharacter : public ACharacter
@@ -20,7 +21,28 @@ public:
 protected:
 UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 USpringArmComponent* SpringArmComp;
-
 UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 UCameraComponent* Cameracomp;
+
+UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+float NormalSpeed;
+UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+float SprintSpeedMultiplier;
+UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+float SprintSpeed;
+
+virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+UFUNCTION()
+void Move(const FInputActionValue& value);
+UFUNCTION()
+void StartJump(const FInputActionValue& value);
+UFUNCTION()
+void StopJump(const FInputActionValue& value);
+UFUNCTION()
+void Look(const FInputActionValue& value);
+UFUNCTION()
+void StartSprint(const FInputActionValue& value);
+UFUNCTION()
+void StopSprint(const FInputActionValue& value);
 };
